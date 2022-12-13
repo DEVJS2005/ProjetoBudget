01f622c9865e1cebe1bb15bb3f6a5175803e5469
DEVJS2005 <jvl14533@outlook.com>
2022-12-13 00:27:47 -0300

    Crud TipoItem
    Crud de tipo item pronto para testes.

---
+++ Controllers/TipoItemController.cs
@@ -0,0 +1,60 @@
+﻿using ProjetoBudget.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoBudget.Controllers
+{
+    public class TipoItemController : Controller
+    {
+        BDBudgetEntities db = new BDBudgetEntities();
+        // GET: TipoItem
+        [HttpGet]
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastro(string nomeTipoItem)
+        {
+            TipoItem TPI = new TipoItem();  
+            TPI.nomeTipoItem= nomeTipoItem;
+            db.TipoItem.Add(TPI);
+            db.SaveChanges();
+
+            return RedirectToAction("listaItens");
+        }
+        
+        public ActionResult ListaItens()
+        {
+            return View(db.TipoItem.ToList());
+        }
+
+        public ActionResult Editar(int idTPI)
+        {
+            return View(db.TipoItem.Find(idTPI)) ;
+        }
+
+        [HttpPost]
+        public ActionResult Editar(int idTPI,string nomeTipoItem)
+        {
+            TipoItem TPI = db.TipoItem.Find(idTPI);
+            TPI.idTipoItem = idTPI;
+            TPI.nomeTipoItem = nomeTipoItem;
+            db.TipoItem.AddOrUpdate(TPI);
+            db.SaveChanges();
+            return RedirectToAction("ListaItens");
+        }
+
+        public ActionResult Excluir(int idTPI)
+        {
+            TipoItem TPI = db.TipoItem.Find(idTPI);
+            db.TipoItem.Remove(TPI);
+            return RedirectToAction("ListaItens");
+        }
+    }
+}
